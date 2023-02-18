48c33766b4fb70859487ad6d101db4dc6e645101
thaafei <fionaxiework@gmail.com>
2023-02-18 15:36:11 -0500

    added google maps api

---
+++ my-app/src/Map.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: 37.7749,
+      lng: -122.4194
+    },
+    zoom: 14
+  };
+
+  render() {
+    return (
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyCgbp4hHoey3OUABV9ViLTapAOYTT_xb4I' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={37.7749}
+            lng={-122.4194}
+            text="Recycling Facility"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default Map;
