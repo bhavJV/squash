593496b1025dcb7ca285430b8ca24deca9368832
thaafei <fionaxiework@gmail.com>
2023-02-18 21:27:06 -0500

    imported fonts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Map from './Map';
+import NavBar from './Navbar';
+import "./App.css"
 
 function App() {
   return (
+    
     <div className="App">
+      <div>
+      <NavBar />
+      </div>
+      
       <h1>Squash</h1>
-      <Map />
     </div>
   );
 }
